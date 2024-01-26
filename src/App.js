26aa9a62a3fb64676afb0059a23ac0d0e529542f
carlosduti70 <cmzuniga@sudamericano.edu.ec>
2024-01-26 08:52:03 -0500

    formulario

--- src/App.js
+++ src/App.js
@@ -1,42 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 
 function App() {
+    // const [emojiString, setEmojiString]= useState("😀")
+  const [character, setCharacter] = useState({
+    name: 'Gandalf',
+    strength: 10,
+    intelligence: 18,
+    charisma: 16
+  });
 
-  const [mostrarEcuador, setMostrarEcuador] = useState(false);
+  useEffect(() => {
+    const storedCharacter = JSON.parse(localStorage.getItem('character'));
+    if (storedCharacter) {
+      setCharacter(storedCharacter);
+    }
+  }, []);
 
-  const mostrarPalabra = () => {
-    setMostrarEcuador(true);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCharacter((prevCharacter) => ({
+      ...prevCharacter,
+      [name]: value
+    }));
   };
 
-  const firstColor=(y)=>{
-    y.stopPropagation();
-    alert('Amarillo')
-  }
-  const secondColor=(b)=>{
-    b.stopPropagation();
-    alert('Azul')
-  }
-  const thirthColor=(r)=>{
-    r.stopPropagation();
-    alert('Rojo')
-  }
+  useEffect(() => {
+    localStorage.setItem('character', JSON.stringify(character));
+  }, [character]);
+
+  const handleSave = () => {
+    // Aquí puedes realizar acciones adicionales antes de guardar si es necesario
+    console.log('Guardar datos:', character);
+  };
 
   return (
-    <div className='yellow' id='yellow' onClick={firstColor}>
-      {mostrarEcuador && (
-        <div>
-          <p>Ecuador</p>
-        </div>
-      )}
-      <div className='blue' id='blue' onClick={secondColor}>
-        <p>Ecuador</p>
-        <div className='red' id='red' onClick={thirthColor}>
-          <p>Ecuador</p>
-        </div>
-      </div>
-    </div>
+
+      <label className="character-form">
+        Name: 
+        <input
+          name= "name"
+          value= {character.name}
+          onChange= {handleChange}
+          />
+
+        Strength:
+        <input
+          name= "strength"
+          value= {character.strength}
+          onChange= {handleChange}
+          />
+
+        Intelligence:
+        <input
+          name= "intelligence"
+          value= {character.intelligence}
+          onChange= {handleChange}
+          />
+        Charisma:
+        <input
+          name= "charisma"
+          value= {character.charisma}
+          onChange= {handleChange}
+          />
+
+        <button className="save-button" onClick={handleSave}>Save data</button>
+      </label>
+
+    // <div>
+    //   <h1>{emojiString}</h1>
+    //   <button onClick={() => {
+    //     setEmojiString(prevEmoji => prevEmoji + "😀");
+    //   }}>Add 1 smiles</button>
+
+    //   <button onClick={() => {
+    //     setEmojiString(prevEmoji => prevEmoji.substring(prevEmoji.length));
+    //   }}>Delete all</button>
+    //   <button onClick={() => {
+    //     setEmojiString(prevEmoji => prevEmoji.substring(2));
+    //   }}>Delete 1  emoji</button>
+
+    // </div>
   );
 }
 
